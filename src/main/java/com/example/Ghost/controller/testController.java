218b151b07330a95a40450943000b59a336050f8
wuleiming <822507481@qq.com>
2019-05-29 10:54:52 +0800

    添加热启动

---
+++ src/main/java/com/example/Ghost/controller/testController.java
@@ -0,0 +1,17 @@
+package com.example.Ghost.controller;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@EnableAutoConfiguration
+@RestController
+@RequestMapping(value = "/test")
+public class testController {
+
+    @RequestMapping(value = "/hello",method = RequestMethod.GET)
+    public String testControler(){
+        return "hello worldaaaa";
+    }
+}
