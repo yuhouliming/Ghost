8a970dc28cc3db71bd06f9b9f37e602cf3191838
yuhouliming <822507481@qq.com>
2020-08-11 15:10:57 +0800

    学习springcloud--添加注册服务中心-注册服务A

---
+++ springcloudprovider/src/test/java/com/example/springcloudprovider/SpringcloudproviderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springcloudprovider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudproviderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
